87899adac2151e59991def1e2dfd41956e966bb7
JL2718 <johnlakness@gmail.com>
2017-04-05 16:08:12 +0000

    mainline patch
    xenial patch failed

--- scripts/patch_realsense-ubuntu-L4T-R21.5.sh
+++ scripts/patch_realsense-ubuntu-L4T-R21.5.sh
@@ -19,18 +19,15 @@ require_package libssl-dev
 require_package wget
 
 LINUX_BRANCH=$(uname -r)
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-# Download the latest the linux kernel sources from ubuntu-xenial tree
-#[ ! -d ${kernel_name} ] && git clone git://kernel.ubuntu.com/ubuntu/ubuntu-xenial.git --depth 1
+# Download the latest sources from NVidia
 wget https://developer.nvidia.com/embedded/dlc/l4t-Jetson-TK1-Kernel-Sources-R21-5 -O ~/Downloads/kernel.tbz2
 cd /usr/src/
-tar -jxvf ~/Downloads/kernel.tbz2
+sudo tar -jxvf ~/Downloads/kernel.tbz2
 cd kernel/
 
-echo -e "\e[32mApplying F200 formats patch patch\e[0m"
-patch -p1 < ../"$( dirname "$0" )"/0001-Add-video-formats-for-Intel-real-sense-F200-camera-new.patch
-echo -e "\e[32mApplying ZR300 SR300 and LR200 formats patch\e[0m"
-patch -p1 < ../"$( dirname "$0" )"/0002-LR200-ZR300-and-SR300-Pixel-Formats.patch
+patch -p1 < $DIR/realsense-camera-formats.patch
 
 # Copy configuration
 sudo cp /usr/src/linux-headers-$(uname -r)/.config .
@@ -53,6 +50,6 @@ sudo cp $KBASE/drivers/media/usb/uvc/uvcvideo.ko ~/$LINUX_BRANCH-uvcvideo.ko
 echo -e "\e[32mPatched kernel module created successfully\n\e[0m"
 
 # Load the newly built module(s)
-try_module_insert uvcvideo	~/$LINUX_BRANCH-uvcvideo.ko /lib/modules/`uname -r`/kernel/drivers/media/usb/uvc/uvcvideo.ko
+try_module_insert uvcvideo      ~/$LINUX_BRANCH-uvcvideo.ko /lib/modules/`uname -r`/kernel/drivers/media/usb/uvc/uvcvideo.ko
 
 echo -e "\e[92m\n\e[1mScript has completed successfully. Please consult the installation guide for further instruction.\n\e[0m"
